b060192fc677f2d588637f4591218252f6a0081b
TheMatiaz0 <TheMatiaz0@protonmail.com>
2020-08-22 12:03:35 +0200

    CurrentDay + CurrentTime

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,4 +1,6 @@
-﻿using Cyberultimate.Unity;
+﻿using Cyberultimate;
+using Cyberultimate.Unity;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -7,14 +9,32 @@ using UnityEngine;
 
 public class TimeManager : MonoSingleton<TimeManager>
 {
-    private const float RealSecondsPerIngameDay = 120;
-    private const byte HourPerDay = 24;
-    private const float MinutesPerHour = 60;
-	private Stopwatch t;
+	[SerializeField]
+	private SerializedTimeSpan inGameTimeSpan;
+
+	public TimeSpan CurrentTime { get; private set; }
+
+	public Cint CurrentDay { get; private set; }
 
 	protected void Start()
 	{
-		t = new Stopwatch();
-		t.Start();
+		CurrentTime = new TimeSpan(20, 0, 0);
+		StartCoroutine(TimeCount());
+	}
+
+
+	private IEnumerator TimeCount()
+	{
+		while (true)
+		{
+			yield return Async.Wait(inGameTimeSpan.TotalSeconds);
+			CurrentTime = new TimeSpan(CurrentTime.Hours, CurrentTime.Minutes + 10, CurrentTime.Seconds);
+
+			if (CurrentTime.Days == 1)
+			{
+				CurrentDay++;
+				UnityEngine.Debug.Log(CurrentDay);
+			}
+		}
 	}
 }
