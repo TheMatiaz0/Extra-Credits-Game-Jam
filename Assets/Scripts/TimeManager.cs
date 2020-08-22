5c26c9ae5a0fccc877b68ae8e0cf934319836de6
TheMatiaz0 <TheMatiaz0@protonmail.com>
2020-08-22 11:16:54 +0200

    Fix

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,6 +1,8 @@
 ﻿using Cyberultimate.Unity;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Timers;
 using UnityEngine;
 
 public class TimeManager : MonoSingleton<TimeManager>
@@ -8,6 +10,11 @@ public class TimeManager : MonoSingleton<TimeManager>
     private const float RealSecondsPerIngameDay = 120;
     private const byte HourPerDay = 24;
     private const float MinutesPerHour = 60;
+	private Stopwatch t;
 
-    
+	protected void Start()
+	{
+		t = new Stopwatch();
+		t.Start();
+	}
 }
