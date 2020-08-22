4251a836b7399d6fd980ebc76c5a2e4404660730
Jakub <jakub@cubepotato.eu>
2020-08-22 11:03:53 +0200

    Main Menu

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class Menu : MonoBehaviour
+    {
+        public CanvasGroup canvas;
+        private void Start()
+        {
+            canvas = GetComponent<CanvasGroup>();
+        }
+
+        private void Update()
+        {
+            if (Input.anyKeyDown)
+            {
+                LeanTween.alphaCanvas(canvas, 0, 1f).setEaseInOutCubic().setOnComplete(StartGame);
+            }
+        }
+
+        private void StartGame()
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
