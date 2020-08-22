4820d999595cef99106fb4b49f10bb5a64155572
TheMatiaz0 <TheMatiaz0@protonmail.com>
2020-08-22 15:35:53 +0200

    ResultMenu

---
+++ Assets/Scripts/ResultMenu.cs
@@ -0,0 +1,21 @@
+﻿using Cyberultimate.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultMenu : MonoBehaviour
+{
+	protected void OnEnable()
+	{
+		TimeManager.Instance.StartNewDay();
+		TimeControl.Register(this, 0);
+
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+	}
+
+	protected void OnDisable()
+	{
+		TimeControl.Unregister(this);
+	}
+}
