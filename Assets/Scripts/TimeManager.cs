0fbe747ca225cff717763353615e7151ca604656
TheMatiaz0 <TheMatiaz0@protonmail.com>
2020-08-22 12:29:13 +0200

    Time and Time

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -12,9 +12,26 @@ public class TimeManager : MonoSingleton<TimeManager>
 	[SerializeField]
 	private SerializedTimeSpan inGameTimeSpan;
 
-	public TimeSpan CurrentTime { get; private set; }
+	public TimeSpan CurrentTime 
+	{
+		get { return _CurrentTime; }
+		private set { if (value == _CurrentTime) return; _CurrentTime = value; OnCurrentTimeChange(this, _CurrentTime); }
+	}
 
-	public Cint CurrentDay { get; private set; }
+	private TimeSpan _CurrentTime;
+
+	public event EventHandler<SimpleArgs<TimeSpan>> OnCurrentTimeChange = delegate { };
+
+	public Cint CurrentDay 
+	{
+		get { return _CurrentDay; }
+		private set { if (value == _CurrentDay) return;  _CurrentDay = value; OnCurrentDayChange(this, _CurrentDay); } 
+	}
+
+	public event EventHandler<SimpleArgs<Cint>> OnCurrentDayChange = delegate { };
+
+
+	private Cint _CurrentDay;
 
 	protected void Start()
 	{
@@ -30,11 +47,16 @@ public class TimeManager : MonoSingleton<TimeManager>
 			yield return Async.Wait(inGameTimeSpan.TotalSeconds);
 			CurrentTime = new TimeSpan(CurrentTime.Hours, CurrentTime.Minutes + 10, CurrentTime.Seconds);
 
+			CurrentDay += (Cint)((uint)CurrentTime.Days);
+			UnityEngine.Debug.Log(CurrentDay);
+
+			/*
 			if (CurrentTime.Days == 1)
 			{
 				CurrentDay++;
-				UnityEngine.Debug.Log(CurrentDay);
+
 			}
+			*/
 		}
 	}
 }
