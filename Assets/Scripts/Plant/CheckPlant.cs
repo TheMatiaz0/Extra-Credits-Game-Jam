ea78dbedc2786326dafa04e2911eeff7706013e2
Jatoja123 <fikulatu@gmail.com>
2020-08-22 09:19:31 +0200

    Tasks

--- Assets/Scripts/Plant/CheckPlant.cs
+++ Assets/Scripts/Plant/CheckPlant.cs
@@ -14,25 +14,30 @@ public class CheckPlant : InteractableObject
 
     protected override void OnInteract()
     {
+        TaskManager task = TaskManager.instance;
         if (plant.Water <= maximumNeedsToMakeATask)
         {
             //add task needs water
             Debug.Log("Needs water!");
+            task.AddTask("Get water for plant!");
         }
         if (plant.Soil <= maximumNeedsToMakeATask)
         {
             //add task needs soil
             Debug.Log("Needs soil!");
+            task.AddTask("Get soil for plant!");
         }
         if (plant.FreshAir <= maximumNeedsToMakeATask)
         {
             //add task needs air
             Debug.Log("Needs air!");
+            task.AddTask("Give the plant fresh air!");
         }
         if (plant.Sunlight <= maximumNeedsToMakeATask)
         {
             //add task needs sun
             Debug.Log("Needs sun!");
+            task.AddTask("Give the plant some sunlight!");
         }
     }
 }
---
+++ Assets/Scripts/UI/TaskManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TaskManager : MonoBehaviour
+{
+    public static TaskManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private Dictionary<string, GameObject> tasks = new Dictionary<string,GameObject>();
+    public GameObject textObj;
+
+    public void AddTask(string txt)
+    {
+        if (!tasks.ContainsKey(txt))
+        {
+            GameObject current = Instantiate(textObj, Vector3.zero, Quaternion.identity, transform);
+            current.GetComponent<Text>().text = txt;
+            tasks.Add(txt,current);
+        }
+    }
+
+    public void RemoveTask(string txt)
+    {
+        if (tasks.ContainsKey(txt))
+        {
+            Animator anim = tasks[txt].GetComponent<Animator>();
+            anim.Play("FadeOut");
+            Destroy(tasks[txt], anim.GetCurrentAnimatorStateInfo(0).length);
+
+            tasks.Remove(txt);
+        }
+    }
+}
