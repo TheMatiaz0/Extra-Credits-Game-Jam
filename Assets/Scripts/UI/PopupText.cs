1d4887c9bb34242211c4217d954aec18e0e4a42f
Jatoja123 <fikulatu@gmail.com>
2020-08-22 09:58:53 +0200

    Popup animation

---
+++ Assets/Scripts/UI/PopupText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupText : MonoBehaviour
+{
+    public void ShowText(string txt, float duration)
+    {
+        GetComponent<Text>().text = txt;
+        GetComponent<Animator>().Play("PopupFadeOut");
+    }
+}
