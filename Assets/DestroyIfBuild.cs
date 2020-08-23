c3c7f2c00170a905f441230c489301adc3abee18
TheMatiaz0 <TheMatiaz0@protonmail.com>
2020-08-23 20:52:44 +0200

    Cleaning up

---
+++ Assets/DestroyIfBuild.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UI;
+using UnityEngine;
+
+public class DestroyIfBuild : MonoBehaviour
+{
+#if !UNITY_EDITOR
+	protected void Awake()
+	{
+		Destroy(this.gameObject);
+	}
+#endif
+}
