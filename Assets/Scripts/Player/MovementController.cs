45fb619934b1e34789180d41743aee925aa724e8
TheMatiaz0 <TheMatiaz0@protonmail.com>
2020-08-23 17:49:13 +0200

    ++

--- Assets/Scripts/Player/MovementController.cs
+++ Assets/Scripts/Player/MovementController.cs
@@ -74,7 +74,7 @@ public class MovementController : MonoSingleton<MovementController>
 
         if ((x > 0.5f || z > 0.5f) && lastStepTime < DateTime.Now.Subtract(TimeSpan.FromMilliseconds(500)))
         {
-            // AudioManager.Instance.PlaySFX(step1 ? "step1" : "step2");
+            AudioManager.Instance.PlaySFX(step1 ? "step1" : "step2");
             step1 = !step1;
             lastStepTime = DateTime.Now;
         }
