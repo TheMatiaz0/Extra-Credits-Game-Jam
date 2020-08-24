d541a07b43b66bf2641a01d44670f12ecee6c8fa
TheMatiaz0 <TheMatiaz0@protonmail.com>
2020-08-24 16:30:20 +0200

    Balancing the gameplay

--- Assets/Scripts/Player/StaminaSystem.cs
+++ Assets/Scripts/Player/StaminaSystem.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 
 public class StaminaSystem : MonoBehaviour
 {
+	[SerializeField]
+	private float startStamina = 100;
 	public LockValue<float> Stamina { get; set; }
 
 	protected void Awake()
 	{
-		Stamina = new LockValue<float>(100, 0, 100);
+		Stamina = new LockValue<float>(startStamina, 0, startStamina);
 	}
 }
