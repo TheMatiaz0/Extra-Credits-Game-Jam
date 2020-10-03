25a0304ca76486354a28ca3c43e5af38313b3a04
Jatoja123 <fikulatu@gmail.com>
2020-10-03 23:36:24 +0200

    Player lose health event

---
+++ Assets/Scripts/Events/PlayerEvents.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEvents : EventLogic
+{
+
+    public float healthLost = 20;
+
+    public override void Activate()
+    {
+
+        var rnd = Random.Range(0, 1);
+
+        var msg = "";
+
+        switch (rnd)
+        {
+            case 0:
+                GameManager.Instance.HealthSys.Health.TakeValue(healthLost, "Event");
+                msg = "You felt bad at night - you lost some health.";
+                break;
+        }
+
+        Debug.Log("plant disease");
+
+        UIManager.Instance.ShowDialogText(msg);
+    }
+}
